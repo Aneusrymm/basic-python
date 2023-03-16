c012c463705b35f85df06a22a87e02b8e49f0cd2
Aneusrymm <113882172+Aneusrymm@users.noreply.github.com>
2023-03-16 19:42:17 +0700

    Add files via upload

---
+++ awal = 1.py
@@ -0,0 +1,5 @@
+awal = 1
+
+while awal <= 10:
+    print(awal)
+    awal = awal + 1
---
+++ pacar_saya = ['agnes','angelica','naomi'.py
@@ -0,0 +1,7 @@
+pacar_saya = ['agnes','angelica','naomi']
+
+print(pacar_saya)
+print(len(pacar_saya))
+
+for pacar in pacar_saya:
+    print(pacar)

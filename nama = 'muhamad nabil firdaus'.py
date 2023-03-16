737a1cd5f31c28868212900e5acc58f5f0e49bd2
Aneusrymm <113882172+Aneusrymm@users.noreply.github.com>
2023-03-16 19:32:42 +0700

    Add files via upload

---
+++ nama = 'muhamad nabil firdaus'.py
@@ -0,0 +1,12 @@
+nama = 'muhamad nabil firdaus'
+print('banyaknya huruf a  ',nama ,nama.count('a'))
+
+print('masukan umur')
+usia = input('umur')
+
+
+if int (usia) <= 20 :
+    print("anda masih muda")
+
+else :
+     print('anda sudah tua')
